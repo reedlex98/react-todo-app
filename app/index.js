526ea494cf8e65d8c317385680ec0516d63204a5
Elder <elder_louzada@outlook.com>
2019-08-11 20:35:39 -0300

    first phase update

--- app/index.js
+++ app/index.js
@@ -1,15 +1,29 @@
+// From scratch, initialize the React app
+// Render an <App /> component
+// Create the <App /> component from scratch
+// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you're making a todo list with some hard-coded items on it
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 
-class App extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return(
-        <div>This is a simple React boilerplate</div>)
-    }
+const App = () => {
+    return (
+        <div className="todo">
+            <div>
+                <input type="checkbox" name="task1" value="finished"/>
+                <label htmlFor="task1">First task</label>
+            </div>
+            <div>
+                <input type="checkbox" name="task2" value="finished"/>
+                <label htmlFor="task2">Second task</label>
+            </div>
+            <div>
+                <input type="checkbox" name="task3" value="finished"/>
+                <label htmlFor="task3">Third task</label>
+            </div>
+        </div>
+    )
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
