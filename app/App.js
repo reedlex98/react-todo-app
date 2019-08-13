153b251546e76111852eab2a5f09337270c7cf77
Elder <elder_louzada@outlook.com>
2019-08-12 23:50:41 -0300

    phase 4 update

--- app/App.js
+++ app/App.js
@@ -2,13 +2,22 @@ import React from 'react'
 import TodoItem from './components/TodoItem'
 import todosData from './../database/todosData'
 
-const App = () => {
-    const TodoItems = todosData.map(todoData => <TodoItem key={todoData.id} text={todoData.text} completed={todoData.completed}/>)
-    return (
-        <div className="todo">
-            {TodoItems}
-        </div>
-    )
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data: todosData
+        }
+    }
+
+    render(){   
+        const TodoItems = this.state.data.map(todoData => <TodoItem key={todoData.id} text={todoData.text} completed={todoData.completed}/>)
+        return (
+            <div className="todo">
+                {TodoItems}
+            </div>
+        )
+    }
 }
 
 export default App
--- app/index.js
+++ app/index.js
@@ -23,6 +23,15 @@ I've created a js file with some todos data in it, which I'm imported into this 
 Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
 */
 
+/*
+// PHASE 4
+In the previous iteration of this todo list app, we pulled in todos data from a JSON file and mapped over it to display the todo items.
+
+Eventually we'll want to be able to modify the data, which will only happen if we've "loaded" the data in to the component's state
+
+Challenge: Change the <App /> component into a stateful class component and load the imported `todosData` into state.
+*/
+
 
 import React from 'react'
 import ReactDOM from 'react-dom'
